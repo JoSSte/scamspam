a21b8462a74f899ca6a5d690b920ee0c2e5a4db6
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2021-05-27 20:53:57 +0200

    Made script more robust * Replaced static year and month lists with generators * Introduced flag to test data generation without sending

--- 001 orsted/scam.py
+++ 001 orsted/scam.py
@@ -5,10 +5,16 @@ import string
 import json
 import datetime
 
+send = False
 random.seed = (os.urandom(1024))
+
+#this ear, in short format
+current_year = datetime.datetime.now().year - 2000
+current_month = datetime.datetime.now().month
+
 #variables for random date
-start_date = datetime.date(1940, 1, 1)
-end_date = datetime.date(2003, 1, 1)
+start_date = datetime.date(1940, 1, 1) #this is not longer in the past than it is realistic to be born that year
+end_date = datetime.date(2003, 1, 1) # you have to be 18 to have a credit card
 time_between_dates = end_date - start_date
 days_between_dates = time_between_dates.days
 
@@ -17,18 +23,22 @@ url = 'https://ietarst-jansf7656.wpdevcloud.com/plugins/76g54fgf/24fhj23zet/jyuk
 names = json.loads(open('../data/names.json').read())
 cards = json.loads(open('../data/cards.json').read())
 surnames = json.loads(open('../data/surnames.json').read())
-expiryYear = ["21","23","24"]
-expiryMonth = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+expiryYear = list(range(current_year, current_year + 4)) #since cards are typically valid about 4 years, and they
+expiryMonth = list(range(1,13))
+
+#Check if the selected month is in the past, and fix it if it is so
+if expiryYear == current_year and expiryMonth < current_month:
+    expiryMonth = current_month
 
 for name in names:
     for surname in surnames:
         #Assemble full name
         fullname = name +' ' + surname
-        #generate CVV
+        #Generate CVV
         cvv = ''.join(random.choice(string.digits) for i in range(3))
-        #generate tel
+        #Generate telephone number
         tlf = ''.join(random.choice(string.digits) for j in range(8))
-        #generate random dob
+        #Generate random D.O.B
         random_number_of_days = random.randrange(days_between_dates)
         random_date = start_date + datetime.timedelta(days=random_number_of_days)
         dob = '%02d-%02d-%d' % (random_date.day, random_date.month, random_date.year)
@@ -37,15 +47,19 @@ for name in names:
         # select expiry year        
         aar = random.choice(expiryYear)
         #select expiry month
-        mon = random.choice(expiryMonth)
-
-        r = requests.post(url, allow_redirects=False, data={
-        	    'yy' : fullname,
-                'ii' : card,
-                'rr' : mon, #MM
-                'zz' : aar, #YY
-                'vv' : cvv, # 3-digit CVV
-                'aa' : tlf, # 8-digit telephone number
-                'ss' : dob #dd-mm-yyyy
-           	})
-        print( 'Submitted [%d] fullname: %s card: %s mon: %s aar: %s cvv: %s tlf: %s dob: %s ' % (r.status_code, fullname, card, mon, aar, cvv, tlf, dob))
+        mon = '{:02d}'.format(random.choice(expiryMonth))
+        if send:
+            r = requests.post(url, allow_redirects=False, data={
+            	    'yy' : fullname,
+                    'ii' : card,
+                    'rr' : mon, #MM
+                    'zz' : aar, #YY
+                    'vv' : cvv, # 3-digit CVV
+                    'aa' : tlf, # 8-digit telephone number
+                    'ss' : dob #dd-mm-yyyy
+               	})
+            print( 'Submitted [%d] ' % (r.status_code), end="\t")
+        else:
+            print('Send is disabled.', end="\t")
+        print("Data:\tcard: %s\tmon: %s\taar: %s\tcvv: %s\ttlf: %s\tdob: %s\tfullname: %s " % (card, mon, aar, cvv, tlf, dob, fullname))
+        
